0a8f4fdc9e19e7b34ceedb7f06163639b4589b72
qswaquang <qswaquang@gmail.com>
2022-09-23 14:12:17 +0700

    first p

---
+++ src/main/java/com/example/QuanLyChuyenBay/QuanLyChuyenBayApplication.java
@@ -0,0 +1,13 @@
+package com.example.QuanLyChuyenBay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuanLyChuyenBayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuanLyChuyenBayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/QuanLyChuyenBay/QuanLyChuyenBayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.QuanLyChuyenBay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuanLyChuyenBayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
